5911f655e0263556865b3bc6e74512b54b6e1389
Brett Abramczyk <bjabramczyk@gmail.com>
2021-11-10 17:27:13 +0100

    tweak the emoji styling

--- logger.js
+++ logger.js
@@ -4,7 +4,7 @@ function log(...args) {
   const label = args[1] ? ` ${args[1]}: ` : "";
   let content = args[0];
   const emojiStyles =
-    "background: papayawhip; border-radius:200%; font-size: 3rem; padding: .5rem";
+    "background: papayawhip; border-radius:1rem; font-size: 2rem; padding: .5rem; margin-right: .5rem;";
   const styles =
     "background:#444; color:#bada55; border-radius:2px; font-size: .875rem; padding: .25rem";
   const emoji = randomEmoji();
