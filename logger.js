e0adad3732abe8d88aa3e751f15c808fc3644dd4
Brett Abramczyk <bjabramczyk@gmail.com>
2021-11-10 16:21:05 +0100

    fixup

--- logger.js
+++ logger.js
@@ -8,7 +8,7 @@ function log(...args) {
    * - https://github.com/alexfrankcodes/random-emoji/blob/master/src/emojis.json
    * - https://gist.github.com/ikr7/c72843556ef3a12014c3
    * - https://github.com/kentcdodds/dotfiles/blob/3be88d5145a489ca7d39e6974f3a778a3efbdd06/.zshrc
-   * 
+   *
    * Currently using the kentcdodds list. Could expand it to reduce chances of collisions if/when that issue arises
    */
   function randomEmoji() {
