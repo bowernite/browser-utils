8e780045458af54fac2ae929393e8c76600ba9ab
Brett Abramczyk <bjabramczyk@gmail.com>
2021-12-13 15:15:55 +0100

    change the colors to purple and teal for the border

--- document-no-focus.js
+++ document-no-focus.js
@@ -12,9 +12,14 @@ window.addEventListener("DOMContentLoaded", () => {
     width: 100vw;
     position: fixed;
     z-index: 99999999999;
-    background: linear-gradient(to bottom, red, rgba(255, 0, 0, 0));
+
+    /* Purple -> teal -> white */
+    background: linear-gradient(to bottom, rgba(152, 0, 253, .6) 5%, rgba(96, 255, 246, .6) 50%, rgba(255, 0, 0, 0) 100%);
+    /* Yellow: rgba(249, 255, 100, 1) */
+
     top: 0;
   }
+
   /* .${className} {
     border: 1px solid red;
     box-sizing: border-box
@@ -22,6 +27,8 @@ window.addEventListener("DOMContentLoaded", () => {
   `;
 });
 window.setInterval(() => {
+  if (!document) return;
+
   if (document.hasFocus()) {
     document.body?.classList.remove(className);
   } else {
